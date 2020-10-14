65e5eb25358735b19a8fcc88cc359572c3430e9b
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-10-14 23:19:24 +0300

    Fixed download on app store button size

--- homepage/src/components/common/downloadOnAppStore.jsx
+++ homepage/src/components/common/downloadOnAppStore.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function DownloadOnAppStore(props) {
   return (
     <a href={props.href}>
-      <img src="/img/download-on-app-store.svg" alt="download-on-app-store" />
+      <img src="/img/download-on-app-store.svg" alt="download-on-app-store" style={{ height: "40pt" }}/>
     </a>
   )
 }
