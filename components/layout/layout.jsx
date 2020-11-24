7fb048f9be6c049ce00b95028f64bfdc1af462aa
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-11-24 18:37:45 +0200

    Moved files into next.js structure

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Footer from "../components/footer/footer"
+import Footer from "../footer/footer"
 import { Helmet } from "react-helmet"
 import styles from "./layout.module.css"
 import { useEffect } from 'react';
