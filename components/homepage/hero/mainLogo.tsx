881262abfbf62330f45291582ead59b6dff93cdd
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-12-11 18:05:59 +0200

    Added more of next/image

--- components/homepage/hero/mainLogo.tsx
+++ components/homepage/hero/mainLogo.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image"
+
 import styles from "./styles/mainLogo.module.sass"
 
 export default function MainLogo() {
   return (
-    <img src="/logos/logo_512.png" alt="main logo" className={styles.logo} />
+    <Image src="/logos/logo_512.png" alt="main logo" width={256} height={256} className={styles.logo} />
   )
 }
